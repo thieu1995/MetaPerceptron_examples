b61ad9e8a84b4377538e1ba5a68b7f3ad522d17c
Thieu Nguyen <nguyenthieu2102@gmail.com>
2025-01-09 10:09:17 +0700

    Update statistic results

---
+++ 11_run_metrics_gd.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 23:03, 07/01/2025 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+import pandas as pd
+
+
+def get_metrics(data_name, path_read, path_save):
+
+    df = pd.read_csv(f"{path_read}/df_result.csv")
+
+    # Group by 'model_name' and calculate the mean and standard deviation for each metric
+    result_df = df.groupby("model_name").agg(["mean", "std"])
+
+    # Save the results to a CSV file
+    result_df.to_csv(f"{path_save}/{data_name}/df_metrics_summary.csv")
+
+
+path = "history_gd"
+
+get_metrics(data_name="iris", path_read=f"{path}/iris", path_save=path)
+get_metrics(data_name="breast_cancer", path_read=f"{path}/breast_cancer", path_save=path)
+get_metrics(data_name="digits", path_read=f"{path}/digits", path_save=path)
+get_metrics(data_name="wine", path_read=f"{path}/wine", path_save=path)
+get_metrics(data_name="phoneme", path_read=f"{path}/phoneme", path_save=path)
+get_metrics(data_name="waveform", path_read=f"{path}/waveform", path_save=path)
+get_metrics(data_name="magic_telescope", path_read=f"{path}/magic_telescope", path_save=path)
+get_metrics(data_name="diabetes", path_read=f"{path}/diabetes", path_save=path)
+get_metrics(data_name="boston", path_read=f"{path}/boston", path_save=path)
+get_metrics(data_name="california", path_read=f"{path}/california", path_save=path)
---
+++ 21_run_metrics_lsd.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 08:50, 08/01/2025 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+import pandas as pd
+
+
+def get_metrics(data_name, path_read, path_save):
+
+    df = pd.read_csv(f"{path_read}/df_result.csv")
+
+    # Group by 'model_name' and calculate the mean and standard deviation for each metric
+    result_df = df.groupby("model_name").agg(["mean", "std"])
+
+    # Save the results to a CSV file
+    result_df.to_csv(f"{path_save}/{data_name}/df_metrics_summary.csv")
+
+
+path = "history_lsd"
+
+get_metrics(data_name="cdc_diabetes", path_read=f"{path}/cdc_diabetes", path_save=path)
+get_metrics(data_name="PhiUSIIL", path_read=f"{path}/PhiUSIIL", path_save=path)
+get_metrics(data_name="rt_iot2022", path_read=f"{path}/rt_iot2022", path_save=path)
+get_metrics(data_name="sepsis_survival", path_read=f"{path}/sepsis_survival", path_save=path)
+get_metrics(data_name="skin", path_read=f"{path}/skin", path_save=path)
